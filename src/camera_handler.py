af85e699b6b7fa7b6d307164a10d7ddf10190bb4
Roberto Mario Buelvas <robertobuelvas94@gmail.com>
2020-07-18 23:39:52 -0400

    Created classes to embed video into wx

--- src/camera_handler.py
+++ src/camera_handler.py
@@ -3,17 +3,19 @@ import threading
 
 import numpy as np
 import cv2
+import wx
 
 
 class CameraHandler:
-    def __init__(self, label, width=640, height=480, fps=30):
+    def __init__(self, label, width=640, height=480, fps=15):
         self.label = label
         self.width = width
         self.height = height
         self.fps = fps
-        self.resetCamera()
+        self.is_recording = False
+        self.reset()
 
-    def connectCamera(self, camera_index):
+    def connect(self, camera_index):
         name = (
             "data/HTPP" + datetime.now().strftime("%Y-%m-%d") + self.label[1] + ".mp4"
         )
@@ -24,13 +26,13 @@ class CameraHandler:
         fourcc = cv2.VideoWriter_fourcc(*"mp4v")
         self.out = cv2.VideoWriter(name, fourcc, self.fps, (self.width, self.height))
 
-    def startCameraRecording(self):
+    def startRecording(self, new_thread=False):
         self.is_recording = True
-        if self.camera_thread is None:
-            self.camera_thread = threading.Thread(target=self.useCamera, daemon=True)
+        if new_thread and self.camera_thread is None:
+            self.camera_thread = threading.Thread(target=self.preview, daemon=True)
             self.camera_thread.start()
 
-    def useCamera(self):
+    def preview(self):
         while self.cap.isOpened():
             ret, frame = self.cap.read()
             if ret:
@@ -67,39 +69,170 @@ class CameraHandler:
             else:
                 break
 
-    def stopCameraRecording(self):
+    def stopRecording(self):
         self.is_recording = False
 
-    def disconnectCamera(self):
-        self.stopCameraRecording()
-        self.cap.release()
-        self.out.release()
+    def disconnect(self):
+        self.stopRecording()
+        if self.cap is not None:
+            self.cap.release()
+        if self.out is not None:
+            self.out.release()
         cv2.destroyAllWindows()
-        self.camera_thread.join()
-        self.resetCamera()
+        if self.camera_thread is not None:
+            self.camera_thread.join()
+        self.reset()
 
-    def resetCamera(self):
+    def getSize(self):
+        return self.width, self.height
+
+    def reset(self):
         self.cap = None
         self.out = None
         self.camera_thread = None
 
+    @classmethod
+    def findPorts(cls, initial_number=0, final_number=10):
+        available_ports = []
+        for i in range(initial_number, final_number):
+            try:
+                cap = cv2.VideoCapture(i, cv2.CAP_DSHOW)
+                ret, frame = cap.read()
+            except Exception as e:
+                pass
+            else:
+                cap.release()
+                if ret:
+                    available_ports.append(i)
+        return available_ports
+
+
+class CameraPanel(wx.Panel):
+    def __init__(self, parent, label):
+        # Initialize panel
+        wx.Panel.__init__(self, parent)
+        # Attach CameraHandler
+        self.camera = CameraHandler(label)
+        # Bind event handlers
+        self.timer = wx.Timer(self)
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.Bind(wx.EVT_TIMER, self.NextFrame)
+        # Define other attributes
+        self.bmp = None
+
+    def connect(self, camera_index):
+        self.camera.connect(camera_index)
+        ret, frame = self.camera.cap.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        self.bmp = wx.Bitmap.FromBuffer(self.camera.width, self.camera.height, frame)
+        self.Refresh()
+        self.timer.Start(1000.0 / self.camera.fps)
+
+    def disconnect(self):
+        self.timer.Stop()
+        self.camera.disconnect()
+        self.bmp = None
+
+    def pauseRecording(self):
+        self.camera.stopRecording()
+
+    def resumeRecording(self):
+        self.camera.startRecording()
+
+    def getCapSize(self):
+        return self.camera.getSize()
+
+    def OnPaint(self, event):
+        dc = wx.BufferedPaintDC(self)
+        if self.bmp is not None:
+            dc.DrawBitmap(self.bmp, 0, 0)
+
+    def NextFrame(self, event):
+        if self.camera.cap.isOpened():
+            ret, frame = self.camera.cap.read()
+            if ret:
+                frame = cv2.flip(frame, 1)
+                timestamp = datetime.now().strftime("%H:%M:%S")
+                timed_frame = frame.copy()
+                if self.camera.is_recording:
+                    cv2.putText(
+                        timed_frame,
+                        timestamp,
+                        (0, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        1,
+                        (255, 255, 255),
+                        3,
+                        cv2.LINE_AA,
+                        False,
+                    )
+                    self.camera.out.write(timed_frame)
+                    cv2.putText(
+                        frame,
+                        "Recording",
+                        (450, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        1,
+                        (255, 255, 255),
+                        3,
+                        cv2.LINE_AA,
+                        False,
+                    )
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                self.bmp.CopyFromBuffer(frame)
+                self.Refresh()
+
+
+class CameraFrame(wx.Frame):
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent=parent, title="Camera frame")
+        mainPanel = wx.Panel(self)
+        outerBox = wx.BoxSizer(wx.VERTICAL)
+        upBox = wx.BoxSizer(wx.HORIZONTAL)
+        self.camL = CameraPanel(mainPanel, "cL")
+        self.camR = CameraPanel(mainPanel, "cR")
+        cam_width, cam_height = self.camL.camera.getSize()
+        self.camL.SetSize(cam_width, cam_height)
+        self.camR.SetSize(cam_width, cam_height)
+        upBox.Add(self.camL, proportion=1, flag=wx.EXPAND)
+        upBox.Add(self.camR, proportion=1, flag=wx.EXPAND)
+        downBox = wx.BoxSizer(wx.HORIZONTAL)
+        connect_btn = wx.Button(mainPanel, label="Connect")
+        connect_btn.Bind(wx.EVT_BUTTON, self.OnConnect)
+        disconnect_btn = wx.Button(mainPanel, label="Disconnect")
+        disconnect_btn.Bind(wx.EVT_BUTTON, self.OnDisconnect)
+        pause_btn = wx.Button(mainPanel, label="Pause")
+        pause_btn.Bind(wx.EVT_BUTTON, self.OnPause)
+        resume_btn = wx.Button(mainPanel, label="Resume")
+        resume_btn.Bind(wx.EVT_BUTTON, self.OnResume)
+        downBox.Add(connect_btn, proportion=0)
+        downBox.Add(disconnect_btn, proportion=0)
+        downBox.Add(pause_btn, proportion=0)
+        downBox.Add(resume_btn, proportion=0)
+        outerBox.Add(upBox, proportion=1, flag=wx.EXPAND)
+        outerBox.Add(downBox, proportion=0, flag=wx.ALIGN_CENTER)
+        mainPanel.SetSizer(outerBox)
+        self.SetSize(2 * cam_width + 15, cam_height + 70)
+
+    def OnConnect(self, event):
+        self.camL.connect(0)
+        self.camR.connect(1)
+
+    def OnDisconnect(self, event):
+        self.camL.disconnect()
+        self.camR.disconnect()
+
+    def OnPause(self, event):
+        self.camL.pauseRecording()
+        self.camR.pauseRecording()
+
+    def OnResume(self, event):
+        self.camL.resumeRecording()
+        self.camR.resumeRecording()
+
 
 if __name__ == "__main__":
-    camL = CameraHandler("cL")
-    camR = CameraHandler("cR")
-    camL.connectCamera(0)
-    camR.connectCamera(1)
-    camL.startCameraRecording()
-    camR.startCameraRecording()
-    while True:
-        x = input("Press h to end the test")
-        if x == "h":
-            break
-        elif x == "j":
-            camL.stopCameraRecording()
-            camR.stopCameraRecording()
-        elif x == "k":
-            camL.startCameraRecording()
-            camR.startCameraRecording()
-    camL.disconnectCamera()
-    camR.disconnectCamera()
+    app = wx.App()
+    frame = CameraFrame(None)
+    frame.Show()
+    app.MainLoop()
